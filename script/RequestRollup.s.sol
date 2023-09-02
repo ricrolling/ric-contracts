54873617f847cf43521c4c1ff702f32a8e7623b0
afkbyte <afkbyte@protonmail.com>
2023-09-02 04:51:04 -0400

    comment out incomplete script

--- script/RequestRollup.s.sol
+++ script/RequestRollup.s.sol
@@ -1,16 +1,18 @@
-pragma solidity ^0.8.4;
+// pragma solidity ^0.8.4;
 
-import "forge-std/Script.sol";
-import "../src/RICRegistry.sol";
+// import "forge-std/Script.sol";
+// import "../src/RICRegistry.sol";
 
-contract RICRegistryScript is Script {
-    function setUp() public {}
+// contract RICRegistryScript is Script {
+//     function setUp() public {}
 
-    function run() public {
-        uint256 chainID = 69;
+//     function run() public {
+//         uint256 chainID = 69;
 
-        bytes config = abi.encodePacked("config");
+//         bytes config = abi.encodePacked("config");
 
-        registry.requestRollup(chainID, config);
-    }
-}
+//         RICRegistry registry = RICRegistry("ADDRESS_HERE");
+
+//         registry.requestRollup(chainID, config);
+//     }
+// }
